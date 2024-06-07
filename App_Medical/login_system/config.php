6ed0bdd4cbd248645c2d14031c997ce06b112953
Aziz Khemiri <137231286+AzizKhemiri@users.noreply.github.com>
2024-06-07 14:56:51 +0100

    Update config.php

--- App_Medical/login_system/config.php
+++ App_Medical/login_system/config.php
@@ -2,11 +2,9 @@
 
 $conn = mysqli_connect('localhost','root','','app_medical') ; 
     //die("Erreur de connexion à la base de données. " . mysqli_connect_error());
-//=========== verifier si la cnx a la base de donnee est travaille ou non 
+
 //if (!$con) {
   //  die("Erreur de connexion à la base de données. " . mysqli_connect_error());
 //}
-//===========
-
 
 ?>
